486e0896880fc8d0dc605f84f2d40050e40a4d5d
Judy Wang <judywang.jp@gmail.com>
2026-07-12 23:38:47 +0900

    feat: add pixi.js v8 + untitled-pixi-live2d-engine

--- vue_frontend/vite.config.ts
+++ vue_frontend/vite.config.ts
@@ -9,9 +9,6 @@ import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['@met4citizen/talkinghead'],
-  },
   server: {
     watch: {
       usePolling: true,
